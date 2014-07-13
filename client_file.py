f284fe7bd116fa0b2bbf6c980fe3d245c38a0dcd
Roman Komarov <roman.komarov@msk.action.ru>
2014-07-13 20:34:52 +0400

    add rest functions

--- client_file.py
+++ client_file.py
@@ -9,13 +9,34 @@ print '3 - read'
 print '4 - read multiple'
 print '5 - delete one element'
 print '6 - delete all elements'
-print '7 - close connection'
+print '\' exit\' - close connection'
 print 'Your choice: ',
 
 while True:
   user_choice = raw_input()
+
+  # Add one element
+  if user_choice == str(1):
+    sock.send('1')
+    print 'your key: ',
+    user_key = raw_input()
+    sock.send(user_key)
+    print 'your value: ',
+    user_value = raw_input()
+    sock.send(user_value)
+
+  # Edit one element
+  elif user_choice == str(2):
+    sock.send('2')
+    print 'print KEY of element to edit: ',    
+    user_key = raw_input()
+    sock.send(user_key)
+    print 'your new value: ',
+    user_new_value = raw_input()
+    sock.send(user_new_value)
+
   # Read one element
-  if user_choice == str(3):
+  elif user_choice == str(3):
     print 'pls type key to see value'
     print '<<<',
     sock.send('3')
@@ -26,13 +47,34 @@ while True:
   elif user_choice == str(4):
     sock.send('4')
 
+  elif user_choice == str(5):
+    sock.send('5')
+    print 'print KEY of element to delete: ',    
+    user_key = raw_input()
+    sock.send(user_key)
+
+  elif user_choice == str(6):
+    print 'deleting all items. Are you sure? (y/n)'
+    user_answer = raw_input()
+    if user_answer == 'y':
+      sock.send('6')
+    elif user_answer == 'n':
+      pass
+    else:
+      print 'Y or N!'
+  
+
+
   # Exit (close connection)
-  elif user_choice == str(7):
-    sock.send('exit')
+  elif user_choice == 'exit':
+    sock.send('7')
 
   # IO Error 
   else:
     print 'no such choice'
+    continue
 
   data = sock.recv(1024)
   print '>>>',data
+  if data == 'bye':
+    break
--- server_file.py
+++ server_file.py
@@ -8,12 +8,24 @@ conn, addr = sock.accept()
 print 'successfully connected', addr
 
 my_dict = dict()
-my_dict['Brasil'] = 1
-my_dict['German'] = 7
+#my_dict['Brasil'] = 1
+#my_dict['German'] = 7
 while True:
   data = conn.recv(1024)
 
-  if data == '3':  # show 1 element
+  if data == '1': # add 1 element
+    key_data = conn.recv(1024)
+    value_data = conn.recv(1024)
+    my_dict[key_data] = value_data
+    conn.send('added successfully') 
+
+  elif data == '2': # edit 1 element
+    key_data = conn.recv(1024)
+    value_data = conn.recv(1024)
+    my_dict[key_data] = value_data
+    conn.send('edited successfully')
+
+  elif data == '3':  # show 1 element
     data = conn.recv(1024)
     try:
       a = my_dict[data]
@@ -24,10 +36,23 @@ while True:
   elif data == '4':  # show all elements
     conn.send(str(my_dict))
 
-  elif data == 'exit':  # exit
+  elif data == '5':
+    key_data = conn.recv(1024)
+    try: 
+       del my_dict[key_data]
+       conn.send('successfully deleted')
+    except:
+       conn.send('no such element')
+
+  elif data == '6':
+    my_dict.clear()
+    conn.send('dict is empty, sir')
+
+
+  elif data == '7':  # exit
     print 'exiting...'
     conn.send('bye')
     break
 
 conn.close()
-print 'connection close '
+print 'connection closed'
